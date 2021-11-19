7f181952ef396ec4b00e6fd9cf7894a0f260c29f
ironkeep <74244805+ironkeep@users.noreply.github.com>
2021-11-19 09:19:45 +0100

    Add files via upload

---
+++ Exercise1/src/Application.java
@@ -0,0 +1,15 @@
+import nl.saxion.app.SaxionApp;
+
+import java.awt.*;
+
+public class Application implements Runnable {
+
+    public static void main(String[] args) {
+        SaxionApp.start(new Application(), 200, 200);
+    }
+
+    public void run() {
+        SaxionApp.printLine("Hello World!");
+    }
+
+}
